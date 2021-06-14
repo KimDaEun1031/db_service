5ca9e18c7173b9145833e4b9be4225505d8b9768
KimDaEun1031 <kimde1031@gmail.com>
2021-06-14 19:44:11 +0900

    first commit

---
+++ src/test/java/org/daeun/RestAPI_DBsave/RestApiDBsaveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daeun.RestAPI_DBsave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDBsaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
